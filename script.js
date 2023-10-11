0dd01359fa80f801bb26476a199f49d463ec4ffe
manimonji <manirastgar78@gmail.com>
2023-10-11 16:37:59 +0330

    Database update

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
-let table = d3.select("table");
+let table = $("table");
 
 fetch("https://raw.githubusercontent.com/manimonji/1051-homework/main/takalif.json")
     .then(response => response.json())
     .then((data) => {
-       table.selectAll()
-            .data(data)
-            .enter()
-            .append("tr")
-            .append("td")
-            .text(d => d.course);
+        data.forEach(homework => {
+            table.append(
+                `<tr>
+                    <td>${homework.course}</td>
+                    <td>${homework.description}</td>
+                </tr>`
+            )
+        });
     });
