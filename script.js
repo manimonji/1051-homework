e974a945375ef0c7d0cbc24bd4232969f0dd6c2c
manimonji <manirastgar78@gmail.com>
2023-10-11 22:12:07 +0330

    Updating the data base and adding feature to show dates better

--- script.js
+++ script.js
@@ -3,11 +3,13 @@ let table = $("table");
 fetch("https://raw.githubusercontent.com/manimonji/1051-homework/main/takalif.json")
     .then(response => response.json())
     .then((data) => {
+        console.log(data);
         data.forEach(homework => {
             table.append(
                 `<tr>
                     <td>${homework.course}</td>
                     <td>${homework.description}</td>
+                    <td>${new Date(homework.date).toLocaleString("fa-IR", {weekday:"long",year:"numeric",month:"2-digit","day":"2-digit"})}</td>
                 </tr>`
             )
         });
