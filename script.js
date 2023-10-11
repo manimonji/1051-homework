28c2079d2c2ab3d934496abe870c2dc96134544d
manimonji <manirastgar78@gmail.com>
2023-10-11 16:21:02 +0330

    updating database and adding dynamic table

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-fetch("takalif.json")
+let table = d3.select("table");
+
+fetch("https://raw.githubusercontent.com/manimonji/1051-homework/main/takalif.json")
     .then(response => response.json())
     .then((data) => {
-        console.log(data)
+       table.selectAll()
+            .data(data)
+            .enter()
+            .append("tr")
+            .append("td")
+            .text(d => d.course);
     });
